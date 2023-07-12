cdfad7d5d6cfae848a4d842856c10675ad6423e3
Ethen Pociask <ethen.pociask@coinbase.com>
2023-07-12 13:43:42 -0700

    Pipeline Management Merge Test (#113)

--- internal/etl/pipeline/manager_test.go
+++ internal/etl/pipeline/manager_test.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/base-org/pessimism/internal/core"
 	"github.com/base-org/pessimism/internal/etl/registry"
 	"github.com/base-org/pessimism/internal/mocks"
+	"github.com/base-org/pessimism/internal/state"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
@@ -50,6 +52,58 @@ func Test_Manager(t *testing.T) {
 				assert.Equal(t, c.Type(), register.ComponentType)
 				assert.Equal(t, c.OutputType(), register.DataType)
 
+			},
+		},
+		{
+			name:        "Successful Pipeline Creations",
+			function:    "CreateDataPipeline",
+			description: "CreateDataPipeline should reuse existing pipeline if it exists",
+
+			constructionLogic: func() Manager {
+				reg := registry.NewRegistry()
+				ctrl := gomock.NewController(t)
+
+				ctx := mocks.Context(context.Background(), ctrl)
+
+				ctx = context.WithValue(ctx, core.State, state.NewMemState())
+
+				return NewManager(ctx, NewAnalyzer(reg), reg, NewEtlStore(), NewComponentGraph(), nil)
+			},
+
+			testLogic: func(t *testing.T, m Manager) {
+				pCfg := &core.PipelineConfig{
+					Network:      core.Layer1,
+					DataType:     core.EventLog,
+					PipelineType: core.Live,
+					ClientConfig: &core.ClientConfig{
+						Network:      core.Layer1,
+						PollInterval: time.Hour * 1,
+					},
+				}
+
+				pUUID1, reuse, err := m.CreateDataPipeline(pCfg)
+				assert.NoError(t, err)
+				assert.False(t, reuse)
+				assert.NotEqual(t, pUUID1, core.NilPUUID())
+
+				// Now create a new pipeline with the same config
+				// & ensure that the previous pipeline is reused
+
+				pUUID2, reuse, err := m.CreateDataPipeline(pCfg)
+				assert.NoError(t, err)
+				assert.True(t, reuse)
+				assert.Equal(t, pUUID1, pUUID2)
+
+				// Now run the pipeline
+				err = m.RunPipeline(pUUID1)
+				assert.NoError(t, err)
+
+				// Ensure shutdown works
+				go func() {
+					_ = m.EventLoop()
+				}()
+				err = m.Shutdown()
+				assert.NoError(t, err)
 			},
 		},
 	}
