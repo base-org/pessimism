1b3a1552f3baf010bf920965260389aa232a0d5d
Ethen Pociask <ethen.pociask@coinbase.com>
2023-11-03 02:01:24 -0700

    [epociask/issue-172-version-pinned-devnet-allocs] Clean up script

--- scripts/devnet-allocs.sh
+++ scripts/devnet-allocs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-## Fetch monorepo binary at specific version used by Pessimism
 VERSION=$(cat go.mod | grep ethereum-optimism/optimism | awk '{print $2}' | sed 's/\/v//g')
 VERSION=$(echo ${VERSION} | sed 's/v//g')
 
@@ -8,9 +7,6 @@ REPO_NAME=optimism-$(echo ${VERSION} | sed 's/v//g')
 
 echo "Downloading ${REPO_NAME} ..."
 git clone --branch v${VERSION} https://github.com/ethereum-optimism/optimism.git ${REPO_NAME}
-
-VERSION=$(echo ${VERSION} | sed 's/v//g')
-echo "Version: ${VERSION}"
 cd ${REPO_NAME}
 
 echo "Initializing monorepo..."
@@ -23,7 +19,7 @@ mv packages/contracts-bedrock/deploy-config/devnetL1.json ../.devnet/devnetL1.js
 STATUS=$?
 
 ## Force cleanup of monorepo 
-echo "${STATUS} Cleaning up ${REPO_NAME} repo ..." &&
+echo "${STATUS} Cleaning up ${REPO_NAME} repo ..."
 cd ../ &&
 rm -rf ${REPO_NAME}
 
@@ -33,5 +29,4 @@ if [ $? -eq 0 ] ; then
 else
     echo "Failed to clean up ${REPO_NAME} repo"
     exit ${STATUS}
-
 fi
