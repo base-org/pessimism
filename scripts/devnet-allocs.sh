8c1d72426680fe4de8a7f4b1a0341d3f73998cac
Ethen <ethen.pociask@coinbase.com>
2023-11-06 13:32:49 -0800

    [issue-172] Version pinned devnet allocations (#175)

--- scripts/devnet-allocs.sh
+++ scripts/devnet-allocs.sh
@@ -1,18 +1,32 @@
-## Download and enter the OP monorepo
-echo "Downloading Optimism monorepo..."
-git clone https://github.com/ethereum-optimism/optimism.git
-cd optimism
-git checkout develop
+#!/bin/bash
 
-## Generate devnet allocations and persist them all into .devnet folder
+VERSION=$(cat go.mod | grep ethereum-optimism/optimism | awk '{print $2}' | sed 's/\/v//g')
+VERSION=$(echo ${VERSION} | sed 's/v//g')
+
+REPO_NAME=optimism-$(echo ${VERSION} | sed 's/v//g')
+
+echo "Downloading ${REPO_NAME} ..."
+git clone --branch v${VERSION} https://github.com/ethereum-optimism/optimism.git ${REPO_NAME}
+cd ${REPO_NAME}
+
 echo "Initializing monorepo..."
-make install-geth
-git submodule update --init --recursive
-make devnet-allocs
-mv .devnet ../.devnet
+make install-geth &&
+git submodule update --init --recursive &&
+make devnet-allocs &&
+cp -R .devnet ../. &&
 mv packages/contracts-bedrock/deploy-config/devnetL1.json ../.devnet/devnetL1.json
 
-## Clean up
-echo "Cleaning up..."
-cd ../
-rm -rf optimism
+STATUS=$?
+
+## Force cleanup of monorepo 
+echo "${STATUS} Cleaning up ${REPO_NAME} repo ..."
+cd ../ &&
+rm -rf ${REPO_NAME}
+
+if [ $? -eq 0 ] ; then
+    echo "Successfully cleaned up ${REPO_NAME} repo"
+    exit ${STATUS}
+else
+    echo "Failed to clean up ${REPO_NAME} repo"
+    exit ${STATUS}
+fi
